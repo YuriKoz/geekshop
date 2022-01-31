2c714f3920374885c3e2db86a290ca8deec6f003
YuriKoz <vohuz@bk.ru>
2022-01-31 13:45:50 +0300

    lesson_9. URLs as regexp

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-$5rw9di_jqhuscpkyxi4^zsou&-r=nu=!5v+_ik0arwgo$az3#"
-)
+SECRET_KEY = "django-insecure-$5rw9di_jqhuscpkyxi4^zsou&-r=nu=!5v+_ik0arwgo$az3#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
